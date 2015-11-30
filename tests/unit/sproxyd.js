409db504f41f0199ed322ca81ae7b9442e374fb1
Michael Zapata <michael.zapata@scality.com>
2015-11-30 11:45:07 +0100

    style(tests): use const whenever possible

--- tests/unit/sproxyd.js
+++ tests/unit/sproxyd.js
@@ -1,14 +1,13 @@
 'use strict';
 
 const assert = require('assert');
-const async = require('async');
 const Sproxy = require('../../index');
 
 let savedKeys;
 
 describe('Requesting Sproxyd', function tests() {
     it('should initialize a new sproxyd client', (done) => {
-        let client = new Sproxy({
+        const client = new Sproxy({
             hostname: '127.0.0.1'
         });
         assert.deepStrictEqual(client.opts.hostname, '127.0.0.1');
@@ -20,7 +19,7 @@ describe('Requesting Sproxyd', function tests() {
     it('should put some data via sproxyd', function putTest(done) {
         // The put operation has sometimes a lot of latency
         this.timeout(0);
-        let client = new Sproxy();
+        const client = new Sproxy();
         client.put(new Buffer('test'), (err, keys) => {
             savedKeys = keys;
             done(err);
@@ -28,7 +27,7 @@ describe('Requesting Sproxyd', function tests() {
     });
 
     it('should get some data via sproxyd', done => {
-        let client = new Sproxy();
+        const client = new Sproxy();
         client.get(savedKeys, (err, data) => {
             if (err) { return done(err); }
             assert.deepStrictEqual(data, [ new Buffer('test'), ]);
@@ -37,13 +36,13 @@ describe('Requesting Sproxyd', function tests() {
     });
 
     it('should delete some data via sproxyd', done => {
-        let client = new Sproxy();
+        const client = new Sproxy();
         client.delete(savedKeys, done);
     });
 
     it('should fail getting non existing data', done => {
-        let client = new Sproxy();
-        client.get(savedKeys, (err, data) => {
+        const client = new Sproxy();
+        client.get(savedKeys, (err) => {
             assert.deepStrictEqual(err, new Error(404), 'Doesn\'t fail');
             done();
         });
