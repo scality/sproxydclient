d42cf3ca94207a15dcdd76181cfeaaf91c747264
Jonathan Gramain <jonathan.gramain@scality.com>
2017-06-16 11:37:03 -0700

    bf: tests: cleanup useless loop over 'algo' param
    The 'algo' param set over a fixture loop is not used by sproxyd client
    (not even by the key generation function where I would expect it to be
    used), it seems to be a remains of old behavior, so remove the loop
    and just run each test once.
    
    Also normalize tests by putting initialization code in 'before'
    section and add code to cleanup dummy server after use.

--- tests/unit/sproxyd.js
+++ tests/unit/sproxyd.js
@@ -60,8 +60,7 @@ function handler(req, res) {
                 md[key] = req.headers['x-scal-usermd'];
             }
             req.on('data', data => {
-                server[key] = Buffer
-                   .concat([server[key], data]);
+                server[key] = Buffer.concat([server[key], data]);
             })
             .on('end', () => makeResponse(res, 200, 'OK'));
         }
@@ -97,8 +96,8 @@ assert.deepStrictEqual(client.bootstrap[0][0], '127.0.0.1');
 assert.deepStrictEqual(client.bootstrap[0][1], '9000');
 assert.deepStrictEqual(client.path, '/proxy/arc/');
 
-describe('Create the server', () => {
-    it('Listen', done => {
+describe('Sproxyd client', () => {
+    before('Create the server', done => {
         server = http.createServer(handler).listen(9000);
         server.on('listening', () => {
             done();
@@ -108,105 +107,90 @@ describe('Create the server', () => {
             process.exit(1);
         });
     });
-});
 
-crypto.getHashes().forEach(algo => {
-    describe(`Requesting Sproxyd ${algo}`, () => {
-        before('initialize a new sproxyd client and fake server', done => {
-            parameters.algo = algo;
-            done();
-        });
+    after('Shutdown the server', done => {
+        client.destroy();
+        server.close(done);
+    });
 
-        it('should put some data via sproxyd', done => {
-            const upStream = new stream.Readable;
-            upStream.push(upload);
-            upStream.push(null);
-            client.put(upStream, upload.length, parameters, reqUid,
-                       (err, key) => {
-                           savedKey = key;
-                           done(err);
-                       });
-        });
+    it('should put some data via sproxyd', done => {
+        const upStream = new stream.Readable;
+        upStream.push(upload);
+        upStream.push(null);
+        client.put(upStream, upload.length, parameters, reqUid,
+                   (err, key) => {
+                       savedKey = key;
+                       done(err);
+                   });
+    });
 
-        it('should get some data via sproxyd', done => {
-            client.get(savedKey, undefined, reqUid, (err, stream) => {
-                let ret = Buffer.alloc(0);
-                if (err) {
-                    done(err);
-                } else {
-                    stream.on('data', val => {
-                        ret = Buffer.concat([ret, val]);
-                    });
-                    stream.on('end', () => {
-                        assert.deepStrictEqual(ret, upload);
-                        done();
-                    });
-                }
-            });
-        });
-
-        it('should delete some data via sproxyd', done => {
-            client.delete(savedKey, reqUid, done);
-        });
-
-        it('should fail getting non existing data', done => {
-            client.get(savedKey, undefined, reqUid, err => {
-                const error = new Error(404);
-                error.isExpected = true;
-                error.code = 404;
-                assert.deepStrictEqual(err, error, 'Doesn\'t fail properly');
-                done();
-            });
-        });
-
-        it(`should put some data via sproxyd without ${algo}`, done => {
-            const upStream = new stream.Readable;
-            upStream.push(upload);
-            upStream.push(null);
-            client.put(upStream, upload.length, parameters, reqUid,
-                       (err, key) => {
-                           savedKey = key;
-                           done(err);
-                       });
-        });
-
-        it('should return success when deleting a locked object', done => {
-            client.delete(lockedObjectKey, reqUid, done);
-        });
-
-        it('should put an empty object via sproxyd', done => {
-            savedKey = generateKey();
-            mdHex = generateMD();
-            client.putEmptyObject(savedKey, mdHex, reqUid, err => {
+    it('should get some data via sproxyd', done => {
+        client.get(savedKey, undefined, reqUid, (err, stream) => {
+            let ret = Buffer.alloc(0);
+            if (err) {
                 done(err);
-            });
-        });
-
-        it('Should get the md of the object', done => {
-            client.getHEAD(savedKey, reqUid, (err, data) => {
-                assert.strictEqual(err, null);
-                assert.strictEqual(data, mdHex);
-                done();
-            });
-        });
-
-        it('Get HEAD should return an error', done => {
-            client.getHEAD(generateKey(), reqUid, err => {
-                assert.notStrictEqual(err, null);
-                assert.notStrictEqual(err, undefined);
-                assert.strictEqual(err.code, 404);
-                done();
-            });
+            } else {
+                stream.on('data', val => {
+                    ret = Buffer.concat([ret, val]);
+                });
+                stream.on('end', () => {
+                    assert.deepStrictEqual(ret, upload);
+                    done();
+                });
+            }
         });
     });
-});
 
-describe('Healthcheck', () => {
-    it('should return 200 OK', done => {
-        client.healthcheck(null, (err, response) => {
-            assert.strictEqual(err, null);
-            assert.strictEqual(response.statusCode, 200);
+    it('should delete some data via sproxyd', done => {
+        client.delete(savedKey, reqUid, done);
+    });
+
+    it('should fail getting non existing data', done => {
+        client.get(savedKey, undefined, reqUid, err => {
+            const error = new Error(404);
+            error.isExpected = true;
+            error.code = 404;
+            assert.deepStrictEqual(err, error, 'Doesn\'t fail properly');
             done();
+        });
+    });
+
+    it('should return success when deleting a locked object', done => {
+        client.delete(lockedObjectKey, reqUid, done);
+    });
+
+    it('should put an empty object via sproxyd', done => {
+        savedKey = generateKey();
+        mdHex = generateMD();
+        client.putEmptyObject(savedKey, mdHex, reqUid, err => {
+            done(err);
+        });
+    });
+
+    it('Should get the md of the object', done => {
+        client.getHEAD(savedKey, reqUid, (err, data) => {
+            assert.strictEqual(err, null);
+            assert.strictEqual(data, mdHex);
+            done();
+        });
+    });
+
+    it('Get HEAD should return an error', done => {
+        client.getHEAD(generateKey(), reqUid, err => {
+            assert.notStrictEqual(err, null);
+            assert.notStrictEqual(err, undefined);
+            assert.strictEqual(err.code, 404);
+            done();
+        });
+    });
+
+    describe('Healthcheck', () => {
+        it('Healthcheck should return 200 OK', done => {
+            client.healthcheck(null, (err, response) => {
+                assert.strictEqual(err, null);
+                assert.strictEqual(response.statusCode, 200);
+                done();
+            });
         });
     });
 });
