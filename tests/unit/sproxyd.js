ecd09ae99b7c8f74ffd1edafc7c94ab2cd2c04c8
Alexander Chan <alexander.chan@scality.com>
2023-04-07 12:30:11 -0700

    SPRXCLT-10: wait for socket connection before reading input stream

--- tests/unit/sproxyd.js
+++ tests/unit/sproxyd.js
@@ -5,6 +5,7 @@ const assert = require('assert');
 const crypto = require('crypto');
 const http = require('http');
 const stream = require('stream');
+const async = require('async');
 
 const Sproxy = require('../../index');
 
@@ -132,13 +133,186 @@ clientAssert(clientCustomPath.bootstrap, clientCustomPath.path);
 const clientNonImmutable = new Sproxy({ bootstrap: ['127.0.0.1:9000'] });
 clientAssert(clientNonImmutable.bootstrap, clientNonImmutable.path);
 
+const clientNonImmutableWithFailover = new Sproxy({
+    bootstrap: ['127.0.0.1:9001', '127.0.0.1:9000']
+});
+
 const clientImmutable = new Sproxy({
     bootstrap: ['127.0.0.1:9000'],
     immutable: true,
 });
 clientAssert(clientImmutable.bootstrap, clientImmutable.path);
+const clientImmutableWithFailover = new Sproxy({
+    bootstrap: ['127.0.0.1:9001', '127.0.0.1:9000'],
+    immutable: true,
+});
+
+[
+    [
+        'Sproxyd client immutable',
+        clientImmutable,
+        { 'x-scal-replica-policy': 'immutable' },
+        null,
+        false,
+    ],
+    [
+        'Sproxyd client non-immutable',
+        clientNonImmutable,
+        null,
+       ['x-scal-replica-policy'],
+        false,
+    ],
+    [
+        'Sproxyd client immutable with failover',
+        clientImmutableWithFailover,
+        { 'x-scal-replica-policy': 'immutable' },
+        null,
+        true,
+    ],
+    [
+        'Sproxyd client immutable with failover',
+        clientNonImmutableWithFailover,
+        null,
+        ['x-scal-replica-policy'],
+        true
+    ],
+
+].forEach(([msg, client, expectHeader, expectNonHeader, failover]) => {
+    describe(msg, function () {
+        this.timeout(5000);
+
+        before('Create the server', done => {
+            server = http.createServer(handler).listen(9000);
+            server.on('listening', () => {
+                done();
+            });
+            server.on('error', err => {
+                process.stdout.write(`${err.stack}\n`);
+                process.exit(1);
+            });
+        });
+
+        after('Shutdown the server', done => {
+            client.destroy();
+            server.close(done);
+        });
+
+        beforeEach(() => {
+            // force bootstrap order to failover test
+            client.bootstrap = failover
+                ? [ ['127.0.0.1', '9001'], ['127.0.0.1', '9000'] ]
+                : [ ['127.0.0.1', '9000'] ];
+            client.current = client.bootstrap[0];
+            expectedRequestHeaders = expectHeader;
+            notExpectedRequestHeaders = expectNonHeader;
+        });
+
+        afterEach(() => {
+            expectedRequestHeaders = undefined;
+            notExpectedRequestHeaders = undefined;
+        });
+
+        it('should put some data via sproxyd',done => {
+            const upStream = new stream.PassThrough();
+            upStream.write(upload);
+            upStream.end();
+            client.put(upStream, upload.length, parameters, reqUid,
+                (err, key) => {
+                    savedKey = key;
+                    done(err);
+                });
+        });
+
+        it('should get some data via sproxyd', done => {
+            client.get(savedKey, undefined, reqUid, (err, stream) => {
+                let ret = Buffer.alloc(0);
+                if (err) {
+                    done(err);
+                } else {
+                    stream.on('data', val => {
+                        ret = Buffer.concat([ret, val]);
+                    });
+                    stream.on('end', () => {
+                        assert.deepStrictEqual(ret, upload);
+                        done();
+                    });
+                }
+            });
+        });
+
+        it('should delete some data via sproxyd', done => {
+            client.delete(savedKey, reqUid, done);
+        });
+
+        it('should fail getting non existing data', done => {
+            client.get(savedKey, undefined, reqUid, err => {
+                const error = new Error();
+                error.isExpected = true;
+                error.code = 404;
+                error.retryable = true;
+                assert.deepStrictEqual(err, error,
+                    'Doesn\'t fail properly');
+                done();
+            });
+        });
+
+        it('should return success when deleting a locked object', done => {
+            client.delete(lockedObjectKey, reqUid, done);
+        });
+
+        it('should put an empty object via sproxyd', done => {
+            savedKey = generateKey();
+            mdHex = generateMD();
+            client.putEmptyObject(savedKey, mdHex, reqUid, err => {
+                done(err);
+            });
+        });
+
+        it('Should get the md of the object', done => {
+            client.getHEAD(savedKey, reqUid, (err, data) => {
+                assert.strictEqual(err, null);
+                assert.strictEqual(data, mdHex);
+                done();
+            });
+        });
+
+        it('Get HEAD should return an error', done => {
+            client.getHEAD(generateKey(), reqUid, err => {
+                assert.notStrictEqual(err, null);
+                assert.notStrictEqual(err, undefined);
+                assert.strictEqual(err.code, 404);
+                done();
+            });
+        });
+
+        it('should return success for batch delete', done => {
+            const list = _batchDelKeys(2000);
+            client.batchDelete(list, reqUid, err => {
+                assert.strictEqual(err, null);
+                done();
+            });
+        });
+
+        it('should abort an unfinished request', done => {
+            const upStream = new stream.PassThrough();
+            upStream.write(upload.slice(0, upload.length - 10));
+            setTimeout(() => upStream.destroy(), 500);
+            client.put(upStream, upload.length, parameters, reqUid,
+                err => {
+                    if (err) {
+                        done();
+                    } else {
+                        assert.fail('expected an immediate error from sproxyd');
+                    }
+                });
+        });
+    });
+});
 
 describe('Sproxyd client', () => {
+    const client = new Sproxy({ bootstrap: ['127.0.0.1:9000'] });
+    clientAssert(client.bootstrap, client.path);
+
     before('Create the server', done => {
         server = http.createServer(handler).listen(9000);
         server.on('listening', () => {
@@ -151,128 +325,13 @@ describe('Sproxyd client', () => {
     });
 
     after('Shutdown the server', done => {
-        clientNonImmutable.destroy();
-        clientImmutable.destroy();
+        client.destroy();
         server.close(done);
-    });
-
-    [false, true].forEach(immutable => {
-        let client;
-        describe(immutable ? 'immutable' : 'non-immutable', () => {
-            before(() => {
-                if (immutable) {
-                    client = clientImmutable;
-                    expectedRequestHeaders = {
-                        'x-scal-replica-policy': 'immutable',
-                    };
-                } else {
-                    client = clientNonImmutable;
-                    notExpectedRequestHeaders = ['x-scal-replica-policy'];
-                }
-            });
-            after(() => {
-                expectedRequestHeaders = undefined;
-                notExpectedRequestHeaders = undefined;
-            });
-            it('should put some data via sproxyd', done => {
-                const upStream = new stream.PassThrough();
-                upStream.write(upload);
-                upStream.end();
-                client.put(upStream, upload.length, parameters, reqUid,
-                    (err, key) => {
-                        savedKey = key;
-                        done(err);
-                    });
-            });
-
-            it('should get some data via sproxyd', done => {
-                client.get(savedKey, undefined, reqUid, (err, stream) => {
-                    let ret = Buffer.alloc(0);
-                    if (err) {
-                        done(err);
-                    } else {
-                        stream.on('data', val => {
-                            ret = Buffer.concat([ret, val]);
-                        });
-                        stream.on('end', () => {
-                            assert.deepStrictEqual(ret, upload);
-                            done();
-                        });
-                    }
-                });
-            });
-
-            it('should delete some data via sproxyd', done => {
-                client.delete(savedKey, reqUid, done);
-            });
-
-            it('should fail getting non existing data', done => {
-                client.get(savedKey, undefined, reqUid, err => {
-                    const error = new Error();
-                    error.isExpected = true;
-                    error.code = 404;
-                    assert.deepStrictEqual(err, error,
-                        'Doesn\'t fail properly');
-                    done();
-                });
-            });
-
-            it('should return success when deleting a locked object', done => {
-                client.delete(lockedObjectKey, reqUid, done);
-            });
-
-            it('should put an empty object via sproxyd', done => {
-                savedKey = generateKey();
-                mdHex = generateMD();
-                client.putEmptyObject(savedKey, mdHex, reqUid, err => {
-                    done(err);
-                });
-            });
-
-            it('Should get the md of the object', done => {
-                client.getHEAD(savedKey, reqUid, (err, data) => {
-                    assert.strictEqual(err, null);
-                    assert.strictEqual(data, mdHex);
-                    done();
-                });
-            });
-
-            it('Get HEAD should return an error', done => {
-                client.getHEAD(generateKey(), reqUid, err => {
-                    assert.notStrictEqual(err, null);
-                    assert.notStrictEqual(err, undefined);
-                    assert.strictEqual(err.code, 404);
-                    done();
-                });
-            });
-
-            it('should return success for batch delete', done => {
-                const list = _batchDelKeys(2000);
-                client.batchDelete(list, reqUid, err => {
-                    assert.strictEqual(err, null);
-                    done();
-                });
-            });
-
-            it('should abort an unfinished request', done => {
-                const upStream = new stream.PassThrough();
-                upStream.write(upload.slice(0, upload.length - 10));
-                setTimeout(() => upStream.destroy(), 500);
-                client.put(upStream, upload.length, parameters, reqUid,
-                    err => {
-                        if (err) {
-                            done();
-                        } else {
-                            assert.fail('expected an immediate error from sproxyd');
-                        }
-                    });
-            });
-        });
     });
 
     describe('Healthcheck', () => {
         it('Healthcheck should return 200 OK', done => {
-            clientNonImmutable.healthcheck(null, (err, response) => {
+            client.healthcheck(null, (err, response) => {
                 assert.strictEqual(err, null);
                 assert.strictEqual(response.statusCode, 200);
                 done();
@@ -282,21 +341,86 @@ describe('Sproxyd client', () => {
 
     describe('Get request uid', () => {
         const uids = 'id1:id2:id3';
-        const log = clientNonImmutable.createLogger(uids);
+        const log = client.createLogger(uids);
         const ids = log.getUids();
 
         it('should return first request id without colon', () => {
-            const firstRequestUid = clientNonImmutable._getFirstReqUid(log);
+            const firstRequestUid = client._getFirstReqUid(log);
             assert.notStrictEqual(firstRequestUid, undefined);
             assert.strictEqual(firstRequestUid.indexOf(':'), -1);
             assert.strictEqual(ids[0], firstRequestUid);
         });
 
         it('should return last request id without colon', () => {
-            const lastRequestUid = clientNonImmutable._getLastReqUid(log);
+            const lastRequestUid = client._getLastReqUid(log);
             assert.notStrictEqual(lastRequestUid, undefined);
             assert.strictEqual(lastRequestUid.indexOf(':'), -1);
             assert.strictEqual(ids.pop(), lastRequestUid);
         });
+    });
+});
+
+describe('Sproxyd PUT error handling', function () {
+    this.timeout(10000);
+
+    const client = new Sproxy({
+        bootstrap: ['127.0.0.1:9001', '127.0.0.1:9000']
+    });
+    let serverWithError;
+    let server;
+
+    before('Create the server', done => {
+        client.bootstrap = [
+            ['127.0.0.1', '9001'],
+            ['127.0.0.1', '9000'],
+        ];
+        client.current = client.bootstrap[0];
+        async.series([
+            next => {
+                serverWithError = http.createServer((req, res) => {
+                    const key = req.url.slice(-40);
+                    req.on('data', data => {
+                        req.destroy();
+                    });
+                }).listen(9001);
+                serverWithError.on('listening', () => {
+                    next();
+                });
+                serverWithError.on('error', err => {
+                    process.stdout.write(`${err.stack}\n`);
+                    process.exit(1);
+                });
+            },
+            next => {
+                server = http.createServer(handler).listen(9000);
+                server.on('listening', () => {
+                    next();
+                });
+                server.on('error', err => {
+                    process.stdout.write(`${err.stack}\n`);
+                    process.exit(1);
+                });
+            },
+        ], done);
+
+    });
+
+    after('Shutdown the server', done => {
+        client.destroy();
+        async.series([
+            next => serverWithError.close(next),
+            next => server.close(next),
+        ], done);
+    });
+
+    it('should not failover for inflight PUT request',done => {
+        const upStream = new stream.PassThrough();
+        upStream.write(upload);
+        client.put(upStream, upload.length, parameters, reqUid,
+            err => {
+                assert(err);
+                assert.strictEqual(err.retryable, false);
+                done();
+            });
     });
 });
